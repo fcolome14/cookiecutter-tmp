974f40fc2050713b579da9b449ebd2ec6a8860cc
Ferran Colome <ferrancolomsanz@gmail.com>
2025-01-29 10:42:25 +0100

    Skeleton build up

---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="{{ cookiecutter.package_name }}",
+    version="0.1.0",
+    author="{{ cookiecutter.author_name }}",
+    packages=["app"],
+    install_requires=[],
+)
